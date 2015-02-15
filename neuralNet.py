21b17f500de2505f74be51b9854116dc33e6e6ce
mikpain7 <mik.github@gmail.com>
2015-02-15 16:57:49 +1300

    Commit all these updates
    Now to bug fixing.

--- neuralNet.py
+++ neuralNet.py
@@ -10,6 +10,10 @@ class Neuron(object):
     def __init__(self):
         self.affectors = {}                      # contains a link to a neuron and an associated weight.
         self.value = -1                          # default out-of bounds value.
+
+    def addAffector(self, n):                    # this case comes into play when we add a neuron without specifiying a wiehgt.
+        addAffector(n, random.random())
+
     def addAffector(self, n, weight):
         self.affectors[n] = weight
 
@@ -35,16 +39,70 @@ class Neuron(object):
 class Layer(object):
     def __init__(self):
         self.neuronList = []
+        self.count = 0
+
+    def __init__(self, layerBelow, layerSize):
+        self.neuronList = []
+        self.count = layerSize
+        self.initNeurons(layerSize)
+        self.connectFully(layerBelow)
 
     def addNeuron(self, neuron):
-        self.neuronList.add(neuron)
+        self.neuronList.append(neuron)
+        self.count += 1
+
+    def initNeurons(self, layerSize):
+        for i in range(0,layerSize):
+            self.addNeuron(Neuron())
+
+    def connectFully(self, layerBelow):
+        for i in self.neuronList:
+            for lbNeuron in layerBelow.neuronList:
+                i.addAffector(lbNeuron)         # add with random weight.
+
+    def setValues(self, valueList):
+        if len(valueList) != len(self.neuronList):
+            raise RuntimeError('Error: When setting the values of a layer the number of values is different from number of neurons.')
+        else:
+            for n in self.neuronList:
+                n.setValue(valueList.pop(0))
+
+    def randomizeLayer(self):
+        for n in self.neuronList:
+            n.randomize()
 
     def calcLayer(self):                # Make every Neuron in this layer calculate its new value.
         for n in self.neuronList:
             n.calcValue()
 
 class Net(object):
-    add = 1
+    def __init__(self):
+        self.inputLayer = Layer()
+        self.outputLayer = Layer()
+        self.hiddenLayers = []
+        self.hiddenLayerCount = 0
+
+    def setInputLayer(self, listOfValues):
+        self.inputLayer.setValues(listOfValues)
+
+    def addHiddenLayer(self):
+        if len(self.hiddenLayers) == 0:
+            self.hiddenLayers.append(Layer(self.inputLayer, self.inputLayer.count))
+            self.hiddenLayerCount += 1
+        else:
+            self.hiddenLayers.append(Layer(self.hiddenLayers[hiddenLayerCount-1], self.hiddenLayers[hiddenLayerCount-1].count))
+            self.hiddenLayerCount += 1
+
+    def addOutputLayer(self, outputNeuronCount):
+        self.outputLayer.initNeurons(outputNeuronCount)
+        self.outputLayer.connectFully(self.hiddenLayers[hiddenLayerCount-1])
+
+    def randomizeNet(self):
+        self.inputLayer.randomizeLayer()
+        for layer in self.hiddenLayers:
+            layer.randomizeLayer()
+        self.outputLayer.randomizeLayer()
+
 
 
 
